7d5313b394f10a9cc6d257b9cc1145e0d5b5498a
VanshajX <vanshsaxena191@gmail.com>
2025-09-15 19:18:33 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "Vanshaj@gmail.com"
+var accountPassword = "12345"
+accountCity = "Gwalior"
+
+// accountId = 2 not allowed in const
+
+accountEmail = "vs@vs.com"
+accountPassword = "2121121"
+accountCity = "Sagar"
+
+/*
+Prefer not to used var because of issue in functional scope and block scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+ "use strict"; // treat all js code as new version
+ 
+//  alert(3 + 3) we are using nodejs , not browser
+
+// tc39 and mdn we will use to get info
+
+// number => 2 to power 53 
+// bigint 
+// string = ""
+// boolean => true/false
+// null => stand alone value
+// undefined
+// symbol => unique
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null);// object
