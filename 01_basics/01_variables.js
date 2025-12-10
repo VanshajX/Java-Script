43a5a038d079e23de4318efdca619a6b92308d05
VanshajX <vanshsaxena191@gmail.com>
2025-12-10 22:21:53 +0530

    Add accountState variable and update console output

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,7 +2,7 @@ const accountId = 144553
 let accountEmail = "Vanshaj@gmail.com"
 var accountPassword = "12345"
 accountCity = "Gwalior"
-
+let accountState;
 // accountId = 2 not allowed in const
 
 accountEmail = "vs@vs.com"
@@ -13,4 +13,4 @@ accountCity = "Sagar"
 Prefer not to used var because of issue in functional scope and block scope
 */
 
-console.table([accountId, accountEmail, accountPassword, accountCity])
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
